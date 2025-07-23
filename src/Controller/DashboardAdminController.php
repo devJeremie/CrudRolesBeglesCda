e98c19f1e04e56732d120c4d4369802f88964323
devJeremie <chabanaisjeremie@gmail.com>
2025-07-23 14:41:13 +0200

    CrudRoles

---
+++ src/Controller/DashboardAdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin')]
+final class DashboardAdminController extends AbstractController
+{
+    #[Route('/dashboardAdmin', name: 'app_dashboard_admin')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function dashboardAdmin(): Response
+    {
+        return $this->render('dashboard_admin/index.html.twig', [
+            'controller_name' => 'DashboardAdminController',
+        ]);
+    }
+}
